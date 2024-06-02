01cbedda0a72d17b01da7816967550c8110614b7
Gabri-Sa <158731669+Gabri-Sa@users.noreply.github.com>
2024-06-02 20:41:28 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header img-fluid">
+        <img src={logo} className="App-logo " alt="logo" />
+      </header>
+    </div>
+  );
+}
