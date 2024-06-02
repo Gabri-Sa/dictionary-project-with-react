e82e6f2f8e1b44ed25911d3c6c8d2df2954b4c5a
Gabri-Sa <158731669+Gabri-Sa@users.noreply.github.com>
2024-06-02 20:49:59 +0100

    Added btn

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ export default function App() {
     <div className="App">
       <header className="App-header img-fluid">
         <img src={logo} className="App-logo " alt="logo" />
+        <a href="#" className="btn btn-primary shadow">
+          Hello
+        </a>
       </header>
     </div>
   );
