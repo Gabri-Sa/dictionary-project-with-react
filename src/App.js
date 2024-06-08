c20aebec114666ea46872159d901b927daafd710
Gabri-Sa <158731669+Gabri-Sa@users.noreply.github.com>
2024-06-08 08:53:30 +0100

    Added details to the footer

--- src/App.js
+++ src/App.js
@@ -11,8 +11,33 @@ export default function App() {
         </main>
         <br />
         <footer className="text-center">
-          This project was coded by <em>Gabriela Saftoiu</em> and is
-          open-sourced
+          This project was coded by
+          <a
+            href="https://portfolio-gabriela-saftoiu.netlify.app/about"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <em>Gabriela Saftoiu</em>
+          </a>
+          , is open-sourced on
+          <a
+            href="https://github.com/Gabri-Sa/dictionary-project-with-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            GitHub
+          </a>{" "}
+          and hosted on
+          <a
+            href="https://dictionary-project-with-react.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Netlify
+          </a>
         </footer>
       </div>
     </div>
