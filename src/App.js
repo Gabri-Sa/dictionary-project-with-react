8494338b9a90d756af43d00a5ace2acd826c85ff
Gabri-Sa <158731669+Gabri-Sa@users.noreply.github.com>
2024-06-08 18:10:13 +0100

    Added component Synonym

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header img-fluid">Dictionary</header>
+        <header className="App-header img-fluid">Explanatory Dictionary</header>
+        <br />
         <main>
           <Dictionary />
         </main>
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,22 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>;
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition: </strong>
               {definition.definition}
               <br />
+              <strong>Example: </strong>
               <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
