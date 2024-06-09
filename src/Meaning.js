d5fee1712de31c0d4e70e8fb7e198d451729f71d
Gabri-Sa <158731669+Gabri-Sa@users.noreply.github.com>
2024-06-09 22:04:04 +0100

    Corrected Synonyms, adjusted  form

--- src/Meaning.js
+++ src/Meaning.js
@@ -17,6 +17,7 @@ export default function Meaning(props) {
           </div>
         );
       })}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,13 +3,12 @@ import "./Phonetic.css";
 
 export default function Phonetic(props) {
   return (
-    <div className="Phonetics">
+    <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
         Listen:
       </a>
 
-      <span className="text"></span>
-      {props.phonetic.text}
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 }
